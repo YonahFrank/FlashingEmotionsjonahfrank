47f862a09460c0d8d246e7cc758e0b691e46f709
YonahFrank <92943529+YonahFrank@users.noreply.github.com>
2021-10-26 13:24:28 -0400

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,16 @@
-basic.forever(function () {
-	
+input.onButtonPressed(Button.A, function () {
+    for (let index = 0; index < 4; index++) {
+        basic.showIcon(IconNames.Happy)
+        basic.pause(200)
+        basic.clearScreen()
+        basic.pause(200)
+    }
+})
+input.onButtonPressed(Button.B, function () {
+    for (let index = 0; index < 4; index++) {
+        basic.showIcon(IconNames.Sad)
+        basic.pause(200)
+        basic.clearScreen()
+        basic.pause(200)
+    }
 })
